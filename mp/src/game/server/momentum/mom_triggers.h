698e2cd4c691f2358e6d3a15cba55f761f54de9f
xen <24222257+xen-000@users.noreply.github.com>
2020-10-08 15:30:59 +0200

    OnStartTouch -> Touch in trigger_momentum_limitmovement

--- mp/src/game/server/momentum/mom_triggers.h
+++ mp/src/game/server/momentum/mom_triggers.h
@@ -556,7 +556,7 @@ class CTriggerLimitMovement : public CBaseMomentumTrigger
     DECLARE_CLASS(CTriggerLimitMovement, CBaseMomentumTrigger);
 
   public:
-    void OnStartTouch(CBaseEntity *pOther) OVERRIDE;
+    void Touch(CBaseEntity *pOther) OVERRIDE;
     void OnEndTouch(CBaseEntity *pOther) OVERRIDE;
 
     // spawnflags
